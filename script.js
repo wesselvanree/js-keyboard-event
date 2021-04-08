54f34c39ed6875492329b445cb511a36bc2b18dd
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2021-04-08 19:25:28 +0200

    Remove unused css

--- script.js
+++ script.js
@@ -12,54 +12,53 @@ document.body.onkeyup = function (e) {
   code.innerHTML = "'" + e.code + "'";
   which.innerHTML = e.which;
   eLocation.innerHTML = e.location;
-  if (e.key == 'D' && e.ctrlKey == true) {
-    e.preventdefault;
-    if (main.classList.contains("dark-mode")) {
-      main.classList.remove("dark-mode");
-    } else {
-      main.classList.add("dark-mode");
-    }
-  }
-  console.log(e);
-}
 
-// copy when clicking a element
-document.querySelector(".element1").addEventListener("click", function () { copyText('element1') });
-document.querySelector(".element2").addEventListener("click", function () { copyText('element2') });
-document.querySelector(".element3").addEventListener("click", function () { copyText('element3') });
-document.querySelector(".element4").addEventListener("click", function () { copyText('element4') });
+  console.log(e);
+};
+
+// copy when clicking an element
+document.querySelector(".element1").addEventListener("click", function () {
+  copyText("element1");
+});
+document.querySelector(".element2").addEventListener("click", function () {
+  copyText("element2");
+});
+document.querySelector(".element3").addEventListener("click", function () {
+  copyText("element3");
+});
+document.querySelector(".element4").addEventListener("click", function () {
+  copyText("element4");
+});
 
 const selectKey = document.querySelector(".select--key");
 const selectCode = document.querySelector(".select--code");
 const selectWhich = document.querySelector(".select--which");
-const selectLocation = document.querySelector(".select--location")
+const selectLocation = document.querySelector(".select--location");
 
 function copyText(element) {
-
-  if (element == 'element1') {
-    selectKey.innerHTML = 'event.key == ' + key.innerHTML;
+  if (element == "element1") {
+    selectKey.innerHTML = "event.key == " + key.innerHTML;
     copyToClipboard(selectKey.outerText);
     copyLog(selectKey.outerText);
-  } if (element == 'element2') {
-    selectCode.innerHTML = 'event.code == ' + code.innerHTML;
+  } else if (element == "element2") {
+    selectCode.innerHTML = "event.code == " + code.innerHTML;
     copyToClipboard(selectCode.outerText);
     copyLog(selectCode.outerText);
-  } if (element == 'element3') {
-    selectWhich.innerHTML = 'event.which == ' + which.innerHTML;
+  } else if (element == "element3") {
+    selectWhich.innerHTML = "event.which == " + which.innerHTML;
     copyToClipboard(selectWhich.outerText);
     copyLog(selectWhich.outerText);
-  } if (element == 'element4') {
-    selectLocation.innerHTML = 'event.location == ' + eLocation.innerHTML;
+  } else if (element == "element4") {
+    selectLocation.innerHTML = "event.location == " + eLocation.innerHTML;
     copyToClipboard(selectLocation.outerText);
     copyLog(selectLocation.outerText);
   }
-
 }
 
 function copyToClipboard(text) {
   var dummy = document.createElement("input");
   document.body.appendChild(dummy);
-  dummy.setAttribute('value', text);
+  dummy.setAttribute("value", text);
   dummy.select();
   document.execCommand("copy");
   document.body.removeChild(dummy);
