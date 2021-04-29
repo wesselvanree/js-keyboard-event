730782558ddcc194d0e57008f53fb09b6cc59583
Wessel van Ree <44018037+wesselvanree@users.noreply.github.com>
2021-04-29 11:25:27 +0200

    add develop script

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
-import { HelperFunctions } from "./HelperFunctions";
-import { EventHandler } from "./EventHandler";
-import { NotificationHandler } from "./NotificationHandler";
+import { HelperFunctions } from './HelperFunctions';
+import { EventHandler } from './EventHandler';
+import { NotificationHandler } from './NotificationHandler';
 
 export const eventHandler = new EventHandler();
 export const notificationHandler = new NotificationHandler();
 export const helper = new HelperFunctions(notificationHandler);
 
 // Set event listeners
-document.body.onkeyup = function (e) {
+document.body.onkeydown = function (e) {
   console.log(e);
   eventHandler.updateProperties(e);
 };
-document.querySelector(".element1").addEventListener("click", () => {
-  eventHandler.copyText("element1");
+document.querySelector('.element1').addEventListener('click', () => {
+  eventHandler.copyText('element1');
 });
-document.querySelector(".element2").addEventListener("click", () => {
-  eventHandler.copyText("element2");
+document.querySelector('.element2').addEventListener('click', () => {
+  eventHandler.copyText('element2');
 });
-document.querySelector(".element3").addEventListener("click", () => {
-  eventHandler.copyText("element3");
+document.querySelector('.element3').addEventListener('click', () => {
+  eventHandler.copyText('element3');
 });
-document.querySelector(".element4").addEventListener("click", () => {
-  eventHandler.copyText("element4");
+document.querySelector('.element4').addEventListener('click', () => {
+  eventHandler.copyText('element4');
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,12 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  target: ["web", "es5"],
-  mode: "production",
-  entry: "./src/index.js",
+  target: ['web', 'es5'],
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
   },
-  watch: true,
   watchOptions: {
     aggregateTimeout: 200,
     poll: 1000,
